ac292551e48f4f3554ffac3cad6ebd128a4b9d08
Manan Nasir <108210496+mannan49@users.noreply.github.com>
2024-09-10 09:12:45 -0400

    Push Admin Dashboard Code

---
+++ src/components/dashboard/DashboardContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TestInformation from "./TestInformation";
+const MainContent = () => {
+  return (
+    <div className="content flex-grow p-4 overflow-y-auto">
+      <TestInformation />
+    </div>
+  );
+};
+
+export default MainContent;
