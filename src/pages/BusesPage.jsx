8b81ad7911e701645503e903e5717037194716d1
AhmedAfzal2089 <ameafzal@gmail.com>
2024-09-12 09:11:08 +0500

    add css changes

--- src/pages/BusesPage.jsx
+++ src/pages/BusesPage.jsx
@@ -35,7 +35,7 @@ const buses = [
 
 const BusesPage = () => {
     return (
-        <div className="p-6 bg-gray-50 min-h-screen w-full m-5 rounded-xl">
+        <div className="p-6 bg-main min-h-screen w-full m-5 rounded-xl">
             {/* Header */}
             <div className="flex justify-between items-center mb-8">
                 <h1 className="text-2xl font-bold text-gray-800">Available Buses</h1>
@@ -45,9 +45,9 @@ const BusesPage = () => {
             </div>
 
             {/* Bus Cards Grid */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6  ">
                 {buses.map((bus, index) => (
-                    <div key={index} className="bg-white rounded-lg shadow-md p-6">
+                    <div key={index} className="bg-white rounded-lg shadow-lg p-6 transition transform hover:scale-105 hover:shadow-xl">
                         <h2 className="text-lg font-bold text-gray-900">{bus.busNo}</h2>
                         <p className="text-gray-600 mb-2">{bus.route}</p>
                         <p className="text-gray-600">Company: {bus.company}</p>
