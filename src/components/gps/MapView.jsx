8e35de5b191dd73799a58e5d99469a04e4a1a070
mannan49 <mannannasir49@gmail.com>
2024-12-07 21:44:35 +0500

    map animation icon added

--- src/components/gps/MapView.jsx
+++ src/components/gps/MapView.jsx
@@ -1,13 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Polyline,
+  useMap,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import axios from "axios";
 import { apiBaseUrl } from "../apis/setting";
 import { jwtDecode } from "jwt-decode";
+import { FaUserCircle } from "react-icons/fa";
+import { renderToStaticMarkup } from "react-dom/server";
+import L from "leaflet";
+
+const riderIcon = L.icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/252/252025.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
+const driverIcon = L.divIcon({
+  className: "custom-icon",
+  html: renderToStaticMarkup(
+    <FaUserCircle style={{ color: "green", fontSize: "32px" }} />
+  ),
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+});
+
+const AnimatedMapCenter = ({ center }) => {
+  const map = useMap();
+  useEffect(() => {
+    if (center) {
+      map.flyTo(center, map.getZoom(), { duration: 1.5 });
+    }
+  }, [center, map]);
+  return null;
+};
 
 const DriverMap = () => {
   const [driverLocation, setDriverLocation] = useState(null);
   const [riderLocation, setRiderLocation] = useState(null);
+  const driverMarkerRef = useRef(null);
+  const riderMarkerRef = useRef(null);
 
   useEffect(() => {
     // Fetch and update driver location every 5 seconds
@@ -33,6 +70,10 @@ const DriverMap = () => {
             busId: "671e58cb2794757a101bc483",
           });
 
+          if (driverMarkerRef.current) {
+            driverMarkerRef.current.setLatLng([latitude, longitude]);
+          }
+
           setDriverLocation({ lat: latitude, lng: longitude });
         },
         (error) => console.error("Error fetching location: ", error),
@@ -50,6 +91,12 @@ const DriverMap = () => {
       console.log("RIDER DATA", riderData);
 
       if (riderData?.userLatitude && riderData?.userLongitude) {
+        if (riderMarkerRef.current) {
+          riderMarkerRef.current.setLatLng([
+            riderData.userLatitude,
+            riderData.userLongitude,
+          ]);
+        }
         setRiderLocation({
           lat: riderData?.userLatitude,
           lng: riderData?.userLongitude,
@@ -68,7 +115,7 @@ const DriverMap = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ height: "100vh", width: "100%" }}>
       {driverLocation && (
         <MapContainer
           center={driverLocation}
@@ -80,9 +127,19 @@ const DriverMap = () => {
             attribution="&copy; OpenStreetMap contributors"
           />
           {/* Driver Marker */}
-          <Marker position={driverLocation} />
+          <Marker
+            position={driverLocation}
+            icon={riderIcon}
+            ref={driverMarkerRef}
+          />
           {/* Rider Marker */}
-          {riderLocation && <Marker position={riderLocation} />}
+          {riderLocation && (
+            <Marker
+              position={riderLocation}
+              icon={driverIcon}
+              ref={riderMarkerRef}
+            />
+          )}
           {/* Polyline */}
           <Polyline positions={[driverLocation, riderLocation]} color="blue" />
         </MapContainer>
