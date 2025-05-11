fe3e4a25d42f139a7b8930079ffee96bf554d80a
mannan49 <mannannasir49@gmail.com>
2025-05-12 00:27:54 +0500

    scan tickets issue

--- src/components/dashboard/DriversAssignedBuses.jsx
+++ src/components/dashboard/DriversAssignedBuses.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { jwtDecode } from "jwt-decode";
-import { useNavigate } from "react-router-dom";
-import { FaBusAlt } from "react-icons/fa";
-import { CgArrowLongRightC } from "react-icons/cg";
-import { apiBaseUrl } from "../apis/setting";
-import { formatDateToDayMonth, formatTime } from "../utils/HelperFunctions";
+import React, { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
+import { FaBusAlt } from 'react-icons/fa';
+import { CgArrowLongRightC } from 'react-icons/cg';
+import { apiBaseUrl } from '../apis/setting';
+import { formatDateToDayMonth, formatTime } from '../utils/HelperFunctions';
 
 const DriversAssignedBuses = () => {
   const [driverBuses, setDriverBuses] = useState([]);
@@ -16,12 +16,12 @@ const DriversAssignedBuses = () => {
 
   const fetchDriverBuses = async () => {
     try {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token');
       const decodedToken = jwtDecode(token);
       const response = await fetch(`${apiBaseUrl}/bus/bus-advance-search`, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
@@ -30,11 +30,16 @@ const DriversAssignedBuses = () => {
       });
 
       if (!response.ok) {
-        throw new Error("Failed to fetch buses");
+        throw new Error('Failed to fetch buses');
       }
 
       const data = await response.json();
-      setDriverBuses(data.data);
+      const now = new Date();
+      const futureOrTodayBuses = data?.data?.filter(bus => {
+        const endDate = new Date(bus?.endDate);
+        return endDate >= now;
+      });
+      setDriverBuses(futureOrTodayBuses);
     } catch (error) {
       console.error("Error fetching driver's buses:", error);
     }
@@ -47,21 +52,13 @@ const DriversAssignedBuses = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {driverBuses.length === 0 ? (
-            <h1 className="text-center text-lg font-semibold">
-              No assigned buses.
-            </h1>
+            <h1 className="text-center text-lg font-semibold">No assigned buses.</h1>
           ) : (
             driverBuses.map((bus, index) => {
-              const randomImageNumber = Math.floor(Math.random() * 5) + 1;
-              const imageSrc = `https://www.freeiconspng.com/uploads/bus-png-${randomImageNumber}.png`;
-
               return (
-                <div
-                  key={index}
-                  className="bg-main mt-2 p-3 shadow-md rounded-xl"
-                >
+                <div key={index} className="bg-main mt-2 p-3 shadow-md rounded-xl">
                   <p className="text-center font-bold flex justify-center items-center gap-2">
-                    {bus?.admin?.name || "Driver"} <FaBusAlt />
+                    {bus?.admin?.name || 'Driver'} <FaBusAlt />
                   </p>
                   <div className="flex justify-center items-center ml-4 w-full">
                     <p>{bus?.route?.startCity}</p>
@@ -69,26 +66,17 @@ const DriversAssignedBuses = () => {
                     <p>{bus?.route?.endCity}</p>
                   </div>
                   <div className="grid grid-cols-[35%_65%] md:grid-cols-2 items-center">
-                    <img
-                      className="rounded-md"
-                      src={imageSrc}
-                      alt={bus?.route?.endCity}
-                    />
+                    <img className="rounded-md" src={bus?.busDetails?.imageSrc} alt={bus?.route?.endCity} />
                     <div>
                       <div className="flex flex-col gap-4">
                         <div className="ml-4 flex flex-col justify-center items-center">
                           <p className="text-tertiary">Only in</p>
-                          <p className="font-bold">
-                            Rs. {bus?.fare?.actualPrice || "N/A"}
-                          </p>
+                          <p className="font-bold">Rs. {bus?.fare?.actualPrice || 'N/A'}</p>
                           <p className="app-btn text-sm mb-2 text-center">
-                            {formatDateToDayMonth(bus?.date)}{" "}
-                            {formatTime(bus?.departureTime)}
+                            {formatDateToDayMonth(bus?.date)} {formatTime(bus?.departureTime)}
                           </p>
                           <p className="app-btn text-sm">
-                            {bus?.route?.stops?.length === 0
-                              ? "Non Stop"
-                              : `Stops: ${bus?.route?.stops?.length}`}
+                            {bus?.route?.stops?.length === 0 ? 'Non Stop' : `Stops: ${bus?.route?.stops?.length}`}
                           </p>
                         </div>
                       </div>
